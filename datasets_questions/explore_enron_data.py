aae566245e3b6307d618e1654ad04184b55fd452
John Shen <johnzhihaoshen@gmail.com>
2016-10-17 23:08:50 -0700

    explored enron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,40 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+poi = []
+salary_count = 0
+email_count = 0
+total_payments = 0
+total_payments_poi = 0
+for name in enron_data:
+	if enron_data[name]['poi'] is True:
+		poi.append(name)
+	if enron_data[name]['email_address'] != 'NaN':
+		email_count += 1
+	if enron_data[name]['salary'] != 'NaN':
+		salary_count += 1
+	if enron_data[name]['total_payments'] != 'NaN':
+		total_payments += 1
+	if enron_data[name]['total_payments'] == 'NaN' and enron_data[name]['poi'] is True:
+		total_payments_poi += 1
+
+
+
+
+
+# print poi
+print len(poi)
+# print (enron_data["SKILLING JEFFREY K"])
+# print (enron_data["PRENTICE JAMES"])
+# print (enron_data["COLWELL WESLEY"])
+# print (enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+
+# vips = ["SKILLING JEFFREY K", "LAY KENNETH L", "FASTOW ANDREW S"]
+# for name in vips:
+# 	print "total payments for {0}: {1}".format(name, enron_data[name]["total_payments"])
+
+print 'email_count', email_count
+print 'salary_count', salary_count
+print 'total_payments', total_payments
+print 'total_payments_poi', total_payments_poi
 
